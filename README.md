8defec9019e4648bf9d456620a0ec613792fb93c
Anwar12052006 <anwarraza6299@gmail.com>
2025-04-30 01:38:15 +0530

    add changes in landing pages and css

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 -PopX build with React + Vite
--Features
- Landing Page — Engaging UI with smooth navigation.
- User Authentication — Sign up and log in with secure form handling.
- User Profiles — Each user has a customizable profile page.
- Responsive Design — Fully responsive across devices.
+- Features
+ -Landing Page — Engaging UI with smooth navigation.
+ -User Authentication — Sign up and log in with secure form handling.
+ -User Profiles — Each user has a customizable profile page.
+ -Responsive Design — Fully responsive across devices.
--- src/components/Landing_Screen.jsx
+++ src/components/Landing_Screen.jsx
@@ -1,18 +1,29 @@
 
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Landing() {
+  const [clicked, setClicked] = useState(false);
+
+  const handlePageClick = () => {
+    // Set clicked to true to apply color change
+    setClicked(true);
+
+    // After 1 second, revert it back
+    setTimeout(() => {
+      setClicked(false);
+    }, 310); // 1000 ms = 1 second
+  };
  
   return (
-    <div className="landing">
+    <div className="landing" onClick={handlePageClick}>
       <h1>Welcome to PopX</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
       <Link to="/signup">
-        <button className="first" >Create Account</button>
+        <button className={`first ${clicked ? "flash" : ""}`} >Create Account</button>
       </Link>
       <Link to="/login">
-        <button className="second">Already Registered? Login</button>
+        <button className={`second ${clicked ? "flash" : ""}`}>Already Registered? Login</button>
       </Link>
     </div>
   );
